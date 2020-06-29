e03a3a9f3c86afc69bd3016b7a366bd57af53151
烨钧 彭 <pyj2419896606@outlook.com>
2020-06-29 10:43:26 +0800

    pyj

--- HR/Startup.cs
+++ HR/Startup.cs
@@ -7,15 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using EFEntity;
+using Microsoft.Extensions.Configuration;
 
 namespace HR
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            var conStr = configuration.GetConnectionString("SqlServerConnection");
+            services.AddDbContext<HR_DBDbContent>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +38,13 @@ namespace HR
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller=Text}/{action=Index}/{id?}");
             });
         }
     }
